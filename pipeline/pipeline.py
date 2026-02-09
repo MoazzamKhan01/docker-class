3aeb26acca2018cfc046bf96f9383ae82de75a2b
Muhammad Moazzam Khan <154009936+MoazzamKhan01@users.noreply.github.com>
2026-02-09 16:24:21 +0000

    pipeline code working

--- pipeline/pipeline.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pandas as pd
-
-args = sys.argv
-df = pd.DataFrame({'column-a':[1,2,3],'column-b':['a','b','c']})
-df['column-3'] = args[1]
-print(df)
-
-df.to_parquet('output.parquet')
